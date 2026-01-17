a83a6777244366f8b55a776148d0ef7fd7f86d2b
Site14AutomatedBot <administrator@goobstation.com>
2026-01-17 03:56:37 +0000

    chore: Automatically update REUSE headers

--- Modules/Site14/Content.Site14.Client/Blinking/Overlays/BlinkIndicatorOverlay.cs
+++ Modules/Site14/Content.Site14.Client/Blinking/Overlays/BlinkIndicatorOverlay.cs
@@ -1,3 +1,10 @@
+// SPDX-FileCopyrightText: 2026 Site-14 Contributors
+//
+// SPDX-License-Identifier: MPL-2.0
+//
+// Additional Use Restrictions apply:
+// See /LICENSES/SITE14-ADDENDUM.md
+
 using System.Numerics;
 using Content.Site14.Shared.Blinking;
 using Robust.Client.Graphics;
